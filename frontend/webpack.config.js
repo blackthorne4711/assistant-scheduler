db4c0f3ed8593c009476cf43ba46c263b0af24eb
Erik Bleckhorns <erik.bleckhorns@gmail.com>
2022-07-07 14:21:08 +0200

    Rework

---
+++ frontend/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const PATHS = {
+    app: path.resolve(__dirname,'app'),
+    build: path.resolve(__dirname,'build')
+};
+module.exports = {
+    entry: {
+        app: PATHS.app + "/index.js"
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+   resolve: {
+      alias: {
+        'vue': 'vue/dist/vue.common.js'
+      }
+   }
+};
